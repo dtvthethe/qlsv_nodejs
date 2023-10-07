7fc34ca8f5922f3706bb3ce2a9fd4200b3411c02
dtvthe <dtvthe@gmail.com>
2023-10-07 11:31:18 +0700

    student-list-create

--- models/Student.js
+++ models/Student.js
@@ -11,9 +11,24 @@ class Student {
     all = async () => {
         const [rows, fields] = await pool.execute('select * from student');
         // pool.end(); // Ko cần end
+        const students = rows.map(row => this.convertToObject(row));
+        return students;
+    }
 
-        return rows;
+    save = async (data) => {
+        const [result] = await pool.execute('INSERT INTO student VALUE (?, ?, ?, ?)', [
+            null,
+            data.name,
+            data.birthday,
+            data.gender
+        ]);
+
+        return result.insertId
+    }
+
+    convertToObject = (row) => {
+        return new Student(row.id, row.name, row.birthday, row.gender);
     }
 }
 
-module.exports = Student;
+module.exports = new Student();
